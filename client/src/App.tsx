d698fde829a786777c981511a81a577eb7d5015b
Robert Merrill <robgmerrill@gmail.com>
2024-09-24 16:58:02 +0000

    Initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <>
       <div>
-        <h1>Hello world</h1>
+        <h1>Hello worlds</h1>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
